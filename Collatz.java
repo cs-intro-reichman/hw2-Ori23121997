7e18d8e4b242d371466b6f37b6eac6d047a07b74
YamlRUNI <155106022+YamlRUNI@users.noreply.github.com>
2025-11-16 14:50:23 +0200

    Feedback Collatz.java

--- Collatz.java
+++ Collatz.java
@@ -1,3 +1,11 @@
+/*
+Feedback:
+Great work!
+Yam
+*/
+
+
+
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
@@ -38,3 +46,4 @@ public class Collatz {
 
 	}
 }
+
